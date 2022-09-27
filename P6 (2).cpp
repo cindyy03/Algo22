508d111d6ac1722e17cb898f820828d64c6abc52
cindy <cindyyl.263@gmail.com>
2022-09-27 20:32:03 +0700

    Create P6 (2).cpp

---
+++ P6 (2).cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int pil; cout<<"Pil: "; cin>>pil;
+	switch (pil){
+		case 1: cout<<"algoritma"<<endl;
+				cout<<"dan"<<endl;
+				cout<<"Pemprograman"<<endl;
+				break;
+		case 2: cout<<"Matematika"<<endl;
+				cout<<"Diskrit"<<endl;
+				break;
+		default: cout<<"bahasa"<<endl;
+				cout<<"Inggris"<<endl;		
+	}
+}
